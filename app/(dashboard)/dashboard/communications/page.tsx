2ff0aa166f9987782eb24df8c468adb518a8d9d4
Iam-DeepakVel <deepak.vel82@gmail.com>
2024-04-01 22:01:48 +0530

    fix: vercel build fix

--- app/(dashboard)/dashboard/communications/page.tsx
+++ app/(dashboard)/dashboard/communications/page.tsx
@@ -110,8 +110,8 @@ export default function CommunicationsPage() {
                   <SheetHeader>
                     <SheetTitle>Edit Email</SheetTitle>
                     <SheetDescription>
-                      Make changes to your profile here. Click save when you're
-                      done.
+                      {`Make changes to your profile here. Click save when you're
+                      done.`}
                     </SheetDescription>
                   </SheetHeader>
                   <div className="grid gap-4 py-4">
@@ -233,7 +233,7 @@ export default function CommunicationsPage() {
             </div>
           ) : (
             <div className="text-md mt-8">
-              You don't have any communications before!
+              {`You don't have any communications before!`}
             </div>
           )}
         </div>
